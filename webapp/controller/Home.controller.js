42900a845f54b7df49f9c5a4ecc6237a0e749417
Gabriel Valdivia <132011810+JavaAddictedDev@users.noreply.github.com>
2023-11-17 16:09:25 -0300

    chore: initialize SAP UI5 project

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.lab2dev.firstapplication.controller.Home", {
+      onInit: function () {},
+
+      onHelloButtonPress: () => {
+        console.log("Olá");
+      },
+    });
+  }
+);
